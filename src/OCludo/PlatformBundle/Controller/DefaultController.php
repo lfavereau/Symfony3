af98a822f861ce01c75dba164f1f320d39c112f4
Ludovic <Ludovic@Ludo-Laptop>
2018-05-08 12:58:43 +0200

    first commit chapitre 1: premier bundle créé

---
+++ src/OCludo/PlatformBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OCludo\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('OCludoPlatformBundle:Default:index.html.twig');
+    }
+}
---
+++ src/OCludo/PlatformBundle/OCludoPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OCludo\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OCludoPlatformBundle extends Bundle
+{
+}
